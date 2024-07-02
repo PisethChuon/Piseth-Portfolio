25040df0e5a4d66b86e91957838ecd0ade89da8d
PisethChuon <122347263+PisethChuon@users.noreply.github.com>
2024-07-02 22:56:50 +0700

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-  return (
-    <div className=""></div>
-  )
-}
+  return <div className=""></div>;
+};
 
-export default Hero
+export default Hero;
